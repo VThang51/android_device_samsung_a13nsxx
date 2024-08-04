5d7a1f3e143d0ebce13007c21593202d777cd32a
Thang Nguyen <118606753+VThang51@users.noreply.github.com>
2024-08-04 09:36:00 +0700

    Update twrp_a13nsxx.mk

--- twrp_a13nsxx.mk
+++ twrp_a13nsxx.mk
@@ -5,18 +5,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a13x/device.mk)
+$(call inherit-product, device/samsung/a13nsxx/device.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a13x/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a13nsxx/recovery/root,recovery/root)
 
 # Device identifier
-PRODUCT_RELEASE_NAME := a13x
-PRODUCT_DEVICE := a13x
-PRODUCT_NAME := twrp_a13x
+PRODUCT_RELEASE_NAME := a13nsxx
+PRODUCT_DEVICE := a13nsxx
+PRODUCT_NAME := twrp_a13nsxx
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A13
 PRODUCT_MANUFACTURER := samsung
